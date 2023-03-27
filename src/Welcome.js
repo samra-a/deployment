cffca93b13fd602e42b422c24a3c20821efc281c
Samra Afzaal <samraafzaal@samras-air.home>
2023-03-27 11:30:59 +0100

    added react router dependency

---
+++ src/Welcome.js
@@ -0,0 +1,23 @@
+const Welcome = () => {
+
+    return(
+        <>
+            <header>
+                <h1>My Portfolio</h1>
+                <nav>
+                    <ul>
+                        <li>About</li>
+                        <li>Projects</li>
+                        <li>Contact</li>
+                    </ul>
+                </nav>
+            </header>
+            <main>
+                <p>Content will go here.</p>
+            </main>
+        </>
+    )
+
+}
+
+export default Welcome;
